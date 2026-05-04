19e0c9a91f5cefc7b22c6aed6056e31c76e11e9f
Marius Øyen Torsen <mariusoyentorsen@Marius-sin-MacBook-Air.local>
2026-05-04 13:52:03 +0200

    idk

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -75,7 +75,7 @@ export default function Navbar() {
     path === '/' ? pathname === '/' : pathname.startsWith(path);
 
   return (
-    <header className="navbar">
+    <header className="navbar safe-header">
       <div className="navbar-inner">
         <Link to="/" className="navbar-brand">
           <span className="brand-text">Consumd</span>
